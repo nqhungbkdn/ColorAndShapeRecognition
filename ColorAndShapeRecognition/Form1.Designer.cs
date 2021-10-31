10264bfd8958dad90312e8053c88254fef4fcc80
phoenix <nqhungbkdn@gmail.com>
2021-10-31 10:28:59 +0700

    final

--- ColorAndShapeRecognition/Form1.Designer.cs
+++ ColorAndShapeRecognition/Form1.Designer.cs
@@ -33,16 +33,30 @@ namespace ColorAndShapeRecognition
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.reToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.tbRedThreshold = new System.Windows.Forms.TrackBar();
+            this.tbObjectSize = new System.Windows.Forms.TrackBar();
+            this.tbGreenThreshold = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbRedThreshold)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbObjectSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGreenThreshold)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -67,34 +81,16 @@ namespace ColorAndShapeRecognition
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // reToolStripMenuItem
             // 
             this.reToolStripMenuItem.Name = "reToolStripMenuItem";
-            this.reToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.reToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
             this.reToolStripMenuItem.Text = "Recognize";
             this.reToolStripMenuItem.Click += new System.EventHandler(this.reToolStripMenuItem_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(266, 450);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(530, 450);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
             // 
             // splitContainer1
             // 
@@ -109,9 +105,109 @@ namespace ColorAndShapeRecognition
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.pictureBox2);
-            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
-            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.Size = new System.Drawing.Size(800, 266);
+            this.splitContainer1.SplitterDistance = 417;
             this.splitContainer1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(417, 266);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(379, 266);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.splitContainer1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.label3);
+            this.splitContainer2.Panel2.Controls.Add(this.label2);
+            this.splitContainer2.Panel2.Controls.Add(this.label1);
+            this.splitContainer2.Panel2.Controls.Add(this.tbGreenThreshold);
+            this.splitContainer2.Panel2.Controls.Add(this.tbObjectSize);
+            this.splitContainer2.Panel2.Controls.Add(this.tbRedThreshold);
+            this.splitContainer2.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer2.SplitterDistance = 266;
+            this.splitContainer2.TabIndex = 4;
+            // 
+            // tbRedThreshold
+            // 
+            this.tbRedThreshold.Location = new System.Drawing.Point(105, 33);
+            this.tbRedThreshold.Maximum = 255;
+            this.tbRedThreshold.Name = "tbRedThreshold";
+            this.tbRedThreshold.Size = new System.Drawing.Size(695, 45);
+            this.tbRedThreshold.TabIndex = 0;
+            this.tbRedThreshold.Value = 40;
+            this.tbRedThreshold.Scroll += new System.EventHandler(this.tbRedThreshold_Scroll);
+            // 
+            // tbObjectSize
+            // 
+            this.tbObjectSize.Location = new System.Drawing.Point(105, 135);
+            this.tbObjectSize.Maximum = 10000;
+            this.tbObjectSize.Name = "tbObjectSize";
+            this.tbObjectSize.Size = new System.Drawing.Size(695, 45);
+            this.tbObjectSize.TabIndex = 1;
+            this.tbObjectSize.Value = 4000;
+            this.tbObjectSize.Scroll += new System.EventHandler(this.tbObjectSize_Scroll);
+            // 
+            // tbGreenThreshold
+            // 
+            this.tbGreenThreshold.Location = new System.Drawing.Point(105, 84);
+            this.tbGreenThreshold.Maximum = 255;
+            this.tbGreenThreshold.Name = "tbGreenThreshold";
+            this.tbGreenThreshold.Size = new System.Drawing.Size(695, 45);
+            this.tbGreenThreshold.TabIndex = 2;
+            this.tbGreenThreshold.Value = 50;
+            this.tbGreenThreshold.Scroll += new System.EventHandler(this.tbGreenThreshold_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Red Threshold";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Green Threshold";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Object Size";
             // 
             // Form1
             // 
@@ -119,18 +215,26 @@ namespace ColorAndShapeRecognition
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.menuStrip1);
-            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.splitContainer2);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tbRedThreshold)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbObjectSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGreenThreshold)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,9 +246,16 @@ namespace ColorAndShapeRecognition
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem reToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar tbGreenThreshold;
+        private System.Windows.Forms.TrackBar tbObjectSize;
+        private System.Windows.Forms.TrackBar tbRedThreshold;
     }
 }
 
--- ColorAndShapeRecognition/Form1.cs
+++ ColorAndShapeRecognition/Form1.cs
@@ -17,12 +17,12 @@ namespace ColorAndShapeRecognition
 {
     public partial class Form1 : Form
     {
-        Image<Bgr, byte> img;
-        Image<Bgr, byte> imgClone;
+        Image<Bgr, byte> imgOriginal;
+        Image<Bgr, byte> imgResult;
 
-        public static int threshold_Red = 0;
-        public static int threshold_Blue = 0;
-        public static int ObjectSize = 0;
+        public static int threshold_Red = 40;
+        public static int threshold_Green = 50;
+        public static int ObjectSize = 1000;
 
         public Form1()
         {
@@ -36,10 +36,8 @@ namespace ColorAndShapeRecognition
                 OpenFileDialog dialog = new OpenFileDialog();
                 if (dialog.ShowDialog() == DialogResult.OK)
                 {
-                    img = new Image<Bgr, byte>(dialog.FileName);
-                    imgClone = img.Clone();
-                    pictureBox1.Image = img.AsBitmap();
-                    RedProcessing();
+                    imgOriginal = new Image<Bgr, byte>(dialog.FileName);
+                    pictureBox1.Image = imgOriginal.AsBitmap();
                 }
             }
             catch (Exception ex)
@@ -52,6 +50,7 @@ namespace ColorAndShapeRecognition
         {
             Image<Gray, byte> imgSubtract;
             Image<Gray, byte> imgBinary;
+            Image<Bgr, byte> imgClone = imgOriginal.Clone();
             Image<Gray, byte> imgCloneGray;
 
             imgCloneGray = imgClone.Convert<Gray, byte>();
@@ -70,7 +69,7 @@ namespace ColorAndShapeRecognition
 
                     CvInvoke.ApproxPolyDP(contours[i], approx, 0.04 * perimeter, true);
 
-                    CvInvoke.DrawContours(imgClone, contours, i, new MCvScalar(0, 0, 0), 2);
+                    CvInvoke.DrawContours(imgResult, contours, i, new MCvScalar(0, 0, 0), 2);
 
                     var moments = CvInvoke.Moments(contours[i]);
                     int x = (int)(moments.M10 / moments.M00);
@@ -78,27 +77,108 @@ namespace ColorAndShapeRecognition
 
                     if (approx.Size == 3)
                     {
-                        CvInvoke.PutText(img, "Red Triangle", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 0), 2);
+                        CvInvoke.PutText(imgResult, "Red Triangle", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 0), 2);
                     }
                     else if (approx.Size == 4)
                     {
-                        CvInvoke.PutText(img, "Red Rectangle", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 0), 2);
-                    }
-                    else if (approx.Size == 6)
-                    {
-                        CvInvoke.PutText(img, "Red Hexagon", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 0), 2);
+                        CvInvoke.PutText(imgResult, "Red Rectangle", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 0), 2);
                     }
                     else if (approx.Size > 6)
                     {
-                        CvInvoke.PutText(img, "Red Circle", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 0), 2);
+                        CvInvoke.PutText(imgResult, "Red Circle", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 0), 2);
+                    }
+                    else
+                    {
+                        CvInvoke.PutText(imgResult, "This type of red shape was not defined yet", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 0), 2);
                     }
                 }
             }
         }
 
+
+        private void GreenProcessing()
+        {
+            Image<Gray, byte> imgSubtract;
+            Image<Gray, byte> imgBinary;
+            Image<Bgr, byte> imgClone = imgOriginal.Clone();
+            Image<Gray, byte> imgCloneGray;
+
+            imgCloneGray = imgClone.Convert<Gray, byte>();
+            imgSubtract = imgClone[1] - imgCloneGray;
+
+            imgBinary = imgSubtract.ThresholdBinary(new Gray(threshold_Green), new Gray(255));
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            Mat m = new Mat();
+            CvInvoke.FindContours(imgBinary, contours, m, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+            for (int i = 0; i < contours.Size; i++)
+            {
+                if (CvInvoke.ContourArea(contours[i]) >= ObjectSize)
+                {
+                    double perimeter = CvInvoke.ArcLength(contours[i], true);
+                    VectorOfPoint approx = new VectorOfPoint();
+
+                    CvInvoke.ApproxPolyDP(contours[i], approx, 0.04 * perimeter, true);
+
+                    CvInvoke.DrawContours(imgResult, contours, i, new MCvScalar(0, 0, 0), 2);
+
+                    var moments = CvInvoke.Moments(contours[i]);
+                    int x = (int)(moments.M10 / moments.M00);
+                    int y = (int)(moments.M01 / moments.M00);
+
+                    if (approx.Size == 3)
+                    {
+                        CvInvoke.PutText(imgResult, "Green Triangle", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 0), 2);
+                    }
+                    else if (approx.Size == 4)
+                    {
+                        CvInvoke.PutText(imgResult, "Green Rectangle", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 0), 2);
+                    }
+                    else if (approx.Size > 6)
+                    {
+                        CvInvoke.PutText(imgResult, "Green Circle", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 0), 2);
+                    }
+                    else
+                    {
+                        CvInvoke.PutText(imgResult, "This type of Green shape was not defined yet", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 0), 2);
+                    }
+                }
+            }
+        }
+
+
         private void reToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            imgResult = imgOriginal.Clone();
+            RedProcessing();
+            GreenProcessing();
+            pictureBox2.Image = imgResult.AsBitmap();
+        }
 
+        private void tbRedThreshold_Scroll(object sender, EventArgs e)
+        {
+            threshold_Red = tbRedThreshold.Value;
+            imgResult = imgOriginal.Clone();
+            RedProcessing();
+            GreenProcessing();
+            pictureBox2.Image = imgResult.AsBitmap();
+        }
+
+        private void tbGreenThreshold_Scroll(object sender, EventArgs e)
+        {
+            threshold_Green = tbGreenThreshold.Value;
+            imgResult = imgOriginal.Clone();
+            RedProcessing();
+            GreenProcessing();
+            pictureBox2.Image = imgResult.AsBitmap();
+        }
+
+        private void tbObjectSize_Scroll(object sender, EventArgs e)
+        {
+            ObjectSize = tbObjectSize.Value;
+            imgResult = imgOriginal.Clone();
+            RedProcessing();
+            GreenProcessing();
+            pictureBox2.Image = imgResult.AsBitmap();
         }
     }
 }
